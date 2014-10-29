5172948d4870add9d382bf027db98d05ce218ccc
Johannes Heck <johannes@railslove.com>
2014-10-29 17:09:27 +0100

    Initial Commit

---
+++ BeaconMapTests/BeaconMapTests.swift
@@ -0,0 +1,36 @@
+//
+//  BeaconMapTests.swift
+//  BeaconMapTests
+//
+//  Created by Johannes Heck on 29.10.14.
+//  Copyright (c) 2014 Johannes Heck. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class BeaconMapTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
